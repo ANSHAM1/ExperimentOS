ee7abe2cab3adf988d788fc9ef9b1d878d37f97f
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-09-04 14:11:23 +0530

    CodeBase Splitted in Services - [app, worker]

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME         : str = "ExperimentOS"
-    APP_VERSION      : str = "1.1.0"
+    APP_VERSION      : str = "1.2.0"
 
     USER_NAME        : str = ""
 
