a06b961d6f8c8b5c0e7917113e99a1b4621f0bc2
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-09-04 13:14:53 +0530

    Agent Routes [Initialized]

--- app/routes/agent.py
+++ app/routes/agent.py
@@ -5,6 +5,10 @@ from fastapi import APIRouter, Depends
 from app.schemas import ExperimentRequest, ExperimentResponse
 from app.dependency import AuthDependency
 
+from app.rabbitmq import RabbitMQRepository
+from app.core import rabbitmq, get_settings
+
+settings = get_settings()
 
 
 agent_router = APIRouter(
@@ -17,4 +21,9 @@ agent_router = APIRouter(
 @agent_router.post("/experiment", response_model=ExperimentResponse)
 async def agent(req: ExperimentRequest, auth: dict[str, Any] = Depends(AuthDependency.get_auth)):
 
-    return ExperimentResponse(output=f"This is a sample response from the AI agent. You said: {req}")
+    payload: dict[str, object] = {
+        "user_id": auth["sub"],
+        "message": req.model_dump(mode="json"),
+    }
+
+    await RabbitMQRepository(rabbitmq.channel).publish(settings.EXPERIMENT_QUEUE, payload)
