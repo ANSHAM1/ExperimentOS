3036988a65bfc5e92d629ff3fdc72debbbe68122
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-09-12 14:28:10 +0530

    Schema - [Step Enum] - Not Required in New Code Version

--- worker/schema/__init__.py
+++ worker/schema/__init__.py
@@ -1,11 +1,8 @@
-from .enums import Step
-
 from .structured_output import CodeGenOutput, CodeExeOutput, CodeEvalOutput
 
 
 
 __all__ = [
-    "Step",
     "CodeGenOutput",
     "CodeExeOutput",
     "CodeEvalOutput"
